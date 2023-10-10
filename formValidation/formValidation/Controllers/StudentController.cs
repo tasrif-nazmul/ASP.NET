2687acf5e3691acd3ba828a2ad263ff71630e775
tasrif-nazmul <tasrifnaazmul@gmail.com>
2023-10-11 02:07:55 +0600

    form validation

---
+++ formValidation/formValidation/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using formValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace formValidation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(Signup s)
+        {
+            return View(s);
+        }
+    }
+}
